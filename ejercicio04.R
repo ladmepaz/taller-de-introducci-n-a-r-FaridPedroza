7299cc3203cb67f48ac8bcac1f4bef6f9edcb531
FaridPedroza <fapedroza@unal.edu.co>
2024-10-30 08:41:29 -0500

    Update ejercicio04.R

--- ejercicio04.R
+++ ejercicio04.R
@@ -1,6 +1,6 @@
 # Resuelve el ejercicio de acuerdo a lo indicado dentro de la función
 
-solucion <- function(s) {
+solucion <- function() {
   # Asigna valores a 'b' de manera que el valor asignado a 's' sea 20 
   a <- 4 # asigna valor aquí
   b <- 5 # asigna valor aquí
