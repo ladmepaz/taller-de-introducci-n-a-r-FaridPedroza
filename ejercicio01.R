6a5272869e5d1f0b419b0a7ffe5b0ecefa345264
FaridPedroza <fapedroza@unal.edu.co>
2024-10-30 07:09:37 -0500

    Update ejercicio01.R

--- ejercicio01.R
+++ ejercicio01.R
@@ -1,9 +1,9 @@
 # Resuelve el ejercicio de acuerdo a lo indicado dentro de la función
 
-solucion <- function() {
+solucion <- function(s) {
   # Asigna valores a 'a' y 'b' de manera que el valor asignado a 's' sea 100
-  a <- NULL # asigna valor aquí
-  b <- NULL # asigna valor aquí
+  a <- 51 # asigna valor aquí
+  b <- 49 # asigna valor aquí
   s <- a+b
   return(s)
 }
